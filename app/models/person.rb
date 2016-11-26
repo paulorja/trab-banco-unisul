e4260ce3444c4a48ed83c3608f3aa9144bf92a4f
PAULO JANSEN <pauloo.jansen@gmail.com>
2016-11-26 13:49:28 -0200

    add seeds

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,5 @@
 class Person < ActiveRecord::Base
+
+  enum tipo: [:fisica, :juridica]
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Manager.create([
+                   {login: 'gerente1', senha: '123'},
+                   {login: 'gerente2', senha: '123456'},
+               ])
+
+PhysicalPerson.create([
+                          {
+                              cpf: '123',
+                              nome: 'João',
+                              person: Person.create({tipo: Person.tipos[:fisica]}),
+                              address: Address.create
+                          },
+                          {
+                              cpf: '456',
+                              nome: 'Maria',
+                              person: Person.create({tipo: Person.tipos[:fisica]}),
+                              address: Address.create
+                          }
+                      ])
+
+LegalPerson.create([
+                       {
+                           cnpj: '1234',
+                           nome: 'Facebook',
+                           person: Person.create({tipo: Person.tipos[:fisica]}),
+                           address: Address.create
+                       },
+                       {
+                           cnpj: '4567',
+                           nome: 'Google',
+                           person: Person.create({tipo: Person.tipos[:fisica]}),
+                           address: Address.create
+                       }
+                   ])
+
+Agency.create([
+                  {nome: 'Agencia 1', numero: 123},
+                  {nome: 'Agencia 2', numero: 1234},
+                  {nome: 'Agencia 3', numero: 456}
+              ])
