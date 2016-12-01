74527e10fab0217bd9f92d805e0c82f14bbc3481
PAULO JANSEN <pauloo.jansen@gmail.com>
2016-11-30 22:57:13 -0200

    improve accounts

--- app/models/account_operation.rb
+++ app/models/account_operation.rb
@@ -1,7 +1,15 @@
 class AccountOperation < ActiveRecord::Base
+
+  before_create :set_account_saldo
+
   belongs_to :account
   belongs_to :person
 
   enum tipo: [:deposito, :saque]
+
+  def set_account_saldo
+    account.update_attribute(:saldo, account.saldo+amount) if deposito?
+    account.update_attribute(:saldo, account.saldo-amount) if saque?
+  end
 
 end
